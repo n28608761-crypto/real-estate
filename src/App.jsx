6d090381b6b9001c6454ad5bd0be0aec12073542
Nkengnoel23 <nkenglefacdorenoel@gmail.com>
2026-01-13 11:50:09 +0100

    Initial comit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      
+      <h1 className='text-amber-900'>Real Estate</h1>
+      
+      </div>
+  )
+}
+
+export default App
