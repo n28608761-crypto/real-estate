d1871fbd7ee068c0d8219bf8cd857a03fb201feb
Nkengnoel23 <nkenglefacdorenoel@gmail.com>
2026-01-13 12:00:25 +0100

    hhdhd

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <div>
       
       <h1 className='text-amber-900'>Real Estate</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, nemo distinctio pariatur voluptates inventore quam perspiciatis cupiditate adipisci modi fugiat dolorem eaque libero blanditiis cumque consequatur labore consectetur natus dolores!</p>
       
       </div>
   )
